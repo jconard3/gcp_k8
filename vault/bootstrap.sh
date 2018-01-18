af76122fa54112b2920aa269965710c8a6c9cb1f
Jordan Conard <jconard@rsglab.com>
2018-01-18 11:40:46 -0500

    Removing commented out set on vault bootstrap used for testing purposes.

--- vault/bootstrap.sh
+++ vault/bootstrap.sh
@@ -4,7 +4,7 @@ set -e
 set -x
 
 kubectl config use-context gke_main-183300_us-east1-b_main
-#kubectl create -f namespace.yaml
+kubectl create -f namespace.yaml
 kubectl config set-context gcp_main_vault --cluster=gke_main-183300_us-east1-b_main --user=gke_main-183300_us-east1-b_main --namespace=vault
 kubectl config use-context gcp_main_vault
 
